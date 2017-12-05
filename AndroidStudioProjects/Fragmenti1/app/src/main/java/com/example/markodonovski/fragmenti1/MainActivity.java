edb58f65f880c26dbf787f3959add22400507201
Marko Donovski <markodonovski@live.com>
2017-12-05 18:53:45 +0100

    Direkna inicijalizaja na MainActivity 2

--- AndroidStudioProjects/Fragmenti1/app/src/main/java/com/example/markodonovski/fragmenti1/MainActivity.java
+++ AndroidStudioProjects/Fragmenti1/app/src/main/java/com/example/markodonovski/fragmenti1/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.markodonovski.fragmenti1;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.button)
+    Button kopce;
+    @BindView(R.id.textView)
+    TextView text;
+    @BindView(R.id.pager)
+    ViewPager pager;
+    @BindView(R.id.tab)
+    TabLayout tab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setUpViewPager(pager);
+        tab.setupWithViewPager(pager);
     }
+
+    private void setUpViewPager(ViewPager pager) {
+        MyPageAdapter adapter = new MyPageAdapter(this.getSupportFragmentManager());
+
+        pager.setAdapter(adapter);
+    }
+
+
 }
